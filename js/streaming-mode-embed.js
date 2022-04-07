9e15d0cd6fbd6eeffb0ddc5162609fe5bd06d10d
Glitch (plump-soft-attempt) <none>
2022-04-07 11:57:30 -0400

    Update streaming-mode-embed.js

--- js/streaming-mode-embed.js
+++ js/streaming-mode-embed.js
@@ -61,7 +61,52 @@ function pickRandomPerk() {
     customColors();
     loadPerks();
 
-    if (getUrlVars()["type"] == "NULL") {
+//    if (getUrlVars()["type"] == "NULL") {
+
+//        if (getUrlVars()["exclude"] != null) {
+//            var perk_blacklist = getUrlVars()["exclude"].split(",").map(Number);
+//        } else {
+//            perk_blacklist = [];
+//        }
+
+//        if (perk_blacklist.length > (perk_json.perks.length - 4)) {
+
+            // TODO: Error: Not enough perks selected
+
+//        } else {
+//            var sel_perks = [];
+//            while (sel_perks.length < 4) {
+//                var randomnumber = Math.floor(Math.random() * (perk_json.perks.length));
+//                if (perk_blacklist.indexOf(randomnumber) > -1) continue;
+//            if (sel_perks.indexOf(randomnumber) > -1) continue;
+//                sel_perks[sel_perks.length] = randomnumber;
+//            }
+
+//            var i = 0;
+//            while (i < 4) {
+//                var id = 'p' + i.toString();
+//                if (getUrlVars()["bg-url"] != null) {
+//                    document.getElementById(id).style.backgroundImage = "url(" + getUrlVars()["bg-url"] + ")";
+//                } else {
+//                    document.getElementById(id).style.backgroundImage = "url(http://perkroulette.xstarwake.com/css/img/perk_purple.png)";
+//                }
+//                i++;
+
+//            }
+
+//            for (var i = 0; i < 4; i++) {
+//                document.getElementById("pn" + i).innerHTML = perk_json.perks[sel_perks[i]].perk_name;
+//                document.getElementById("pc" + i).innerHTML = perk_json.perks[sel_perks[i]].character;
+//                document.getElementById("pi" + i).style.backgroundImage = "url(http://perkroulette.xstarwake.com/css/img/" + active_type + "/iconperks-" + perk_json.perks[sel_perks[i]].perk_name.toString().toLowerCase().normalize("NFD").replace(/ /gi, '').replace(/'/gi, '').replace(/-/gi, '').replace(/&/gi, 'and').replace(/!/gi, '').replace(/:/gi, '').replace(/\p{Diacritic}/gu, '') + ".png)";
+
+//                document.getElementById("pn" + i).classList.add('transparent');
+//                document.getElementById("pc" + i).classList.add('transparent');
+//                document.getElementById("p" + i).classList.add('transparent');
+//            }
+
+//            window.setTimeout(perk1an, 250);
+//        }
+//    } else {
 
         if (getUrlVars()["exclude"] != null) {
             var perk_blacklist = getUrlVars()["exclude"].split(",").map(Number);
@@ -106,52 +151,7 @@ function pickRandomPerk() {
 
             window.setTimeout(perk1an, 250);
         }
-    } else {
-
-        if (getUrlVars()["exclude"] != null) {
-            var perk_blacklist = getUrlVars()["exclude"].split(",").map(Number);
-        } else {
-            perk_blacklist = [];
-        }
-
-        if (perk_blacklist.length > (perk_json.perks.length - 4)) {
-
-            // TODO: Error: Not enough perks selected
-
-        } else {
-            var sel_perks = [];
-            while (sel_perks.length < 4) {
-                var randomnumber = Math.floor(Math.random() * (perk_json.perks.length));
-                if (perk_blacklist.indexOf(randomnumber) > -1) continue;
-            if (sel_perks.indexOf(randomnumber) > -1) continue;
-                sel_perks[sel_perks.length] = randomnumber;
-            }
-
-            var i = 0;
-            while (i < 4) {
-                var id = 'p' + i.toString();
-                if (getUrlVars()["bg-url"] != null) {
-                    document.getElementById(id).style.backgroundImage = "url(" + getUrlVars()["bg-url"] + ")";
-                } else {
-                    document.getElementById(id).style.backgroundImage = "url(http://perkroulette.xstarwake.com/css/img/perk_purple.png)";
-                }
-                i++;
-
-            }
-
-            for (var i = 0; i < 4; i++) {
-                document.getElementById("pn" + i).innerHTML = perk_json.perks[sel_perks[i]].perk_name;
-                document.getElementById("pc" + i).innerHTML = perk_json.perks[sel_perks[i]].character;
-                document.getElementById("pi" + i).style.backgroundImage = "url(http://perkroulette.xstarwake.com/css/img/" + active_type + "/iconperks-" + perk_json.perks[sel_perks[i]].perk_name.toString().toLowerCase().normalize("NFD").replace(/ /gi, '').replace(/'/gi, '').replace(/-/gi, '').replace(/&/gi, 'and').replace(/!/gi, '').replace(/:/gi, '').replace(/\p{Diacritic}/gu, '') + ".png)";
-
-                document.getElementById("pn" + i).classList.add('transparent');
-                document.getElementById("pc" + i).classList.add('transparent');
-                document.getElementById("p" + i).classList.add('transparent');
-            }
-
-            window.setTimeout(perk1an, 250);
-        }
-    }
+//    }
 }
 
 function perk1an() {
